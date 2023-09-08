365397b44cc5f555b4e42c36aed9cb86b2b37a7b
joseph muganga <jmuganga54@users.noreply.github.com>
2023-09-08 05:31:39 +0300

    Game part 2

--- 15 Boss Level Challenge 2 The Simon Game/Simon Game Challenge Starting Files/game.js
+++ 15 Boss Level Challenge 2 The Simon Game/Simon Game Challenge Starting Files/game.js
@@ -1,12 +1,40 @@
+let userClickedPattern = [];
 let gamePattern = [];
 let buttonColours = ["red", "blue", "green", "yellow"];
 
 //functions
-const nextSequence = () => {
-  let randomNumber = Math.floor(Math.random() * 4);
-  let randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
-  return gamePattern;
+/**
+ * Play sound
+ * @param {*} sound - string audio name
+ */
+const playSound = (name) => {
+  let audio = new Audio(`sounds/${name}.mp3`);
+  audio.play();
 };
 
-console.log(nextSequence());
+/**
+ *
+ */
+const nextSequence = () => {
+  let randomNumber = Math.floor(Math.random() * 4);
+
+  let randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  console.log();
+
+  $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  $(document).ready(function () {
+    $('div[type="button"]').click(function (e) {
+      let userChosenColour = e.target.id;
+      userClickedPattern.push(userChosenColour);
+      console.log(userClickedPattern);
+      playSound(userChosenColour);
+    });
+  });
+  return userClickedPattern;
+};
+
+//Call function
+
+nextSequence();
