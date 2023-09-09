d6d573c999771a19bfc32064e68ffdf6f7329633
joseph muganga <jmuganga54@users.noreply.github.com>
2023-09-09 04:53:31 +0300

    Game part 3

--- 15 Boss Level Challenge 2 The Simon Game/Simon Game Challenge Starting Files/game.js
+++ 15 Boss Level Challenge 2 The Simon Game/Simon Game Challenge Starting Files/game.js
@@ -14,21 +14,44 @@ const playSound = (name) => {
 
 /**
  *
+ * @param {string} currentColour - current color
+ */
+const animatePress = (currentColour) => {
+  debugger;
+  $(`#${currentColour}`).addClass("pressed");
+  setTimeout(() => {
+    $(`#${currentColour}`).removeClass("pressed");
+  }, 100);
+};
+
+/**
+ *
  */
 const nextSequence = () => {
+  //pick a random position number
   let randomNumber = Math.floor(Math.random() * 4);
 
+  //pick a random color based on position
   let randomChosenColour = buttonColours[randomNumber];
+
+  //assign it to array
   gamePattern.push(randomChosenColour);
-  console.log();
 
+  // make a button flash
   $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  console.log(randomChosenColour);
 
+  //addEVentListener when the button is clicked
   $(document).ready(function () {
     $('div[type="button"]').click(function (e) {
       let userChosenColour = e.target.id;
+
       userClickedPattern.push(userChosenColour);
       console.log(userClickedPattern);
+      //add animated background to the button
+      animatePress(userChosenColour);
+
+      //play audio when button is clicked
       playSound(userChosenColour);
     });
   });
