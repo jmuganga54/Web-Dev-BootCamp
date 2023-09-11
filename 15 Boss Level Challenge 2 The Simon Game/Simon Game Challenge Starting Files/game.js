abc84b9632334bca98e3b72dbc7246b05d989c0d
joseph muganga <jmuganga54@users.noreply.github.com>
2023-09-11 06:49:28 +0300

    Step 7 Start the Game part 1

--- 15 Boss Level Challenge 2 The Simon Game/Simon Game Challenge Starting Files/game.js
+++ 15 Boss Level Challenge 2 The Simon Game/Simon Game Challenge Starting Files/game.js
@@ -1,6 +1,8 @@
-let userClickedPattern = [];
-let gamePattern = [];
-let buttonColours = ["red", "blue", "green", "yellow"];
+/** @format */
+
+let userClickedPattern = []
+let gamePattern = []
+let buttonColours = ['red', 'blue', 'green', 'yellow']
 
 //functions
 /**
@@ -8,56 +10,62 @@ let buttonColours = ["red", "blue", "green", "yellow"];
  * @param {*} sound - string audio name
  */
 const playSound = (name) => {
-  let audio = new Audio(`sounds/${name}.mp3`);
-  audio.play();
-};
+  let audio = new Audio(`sounds/${name}.mp3`)
+  audio.play()
+}
 
 /**
  *
  * @param {string} currentColour - current color
  */
 const animatePress = (currentColour) => {
-  debugger;
-  $(`#${currentColour}`).addClass("pressed");
+  $(`#${currentColour}`).addClass('pressed')
   setTimeout(() => {
-    $(`#${currentColour}`).removeClass("pressed");
-  }, 100);
-};
+    $(`#${currentColour}`).removeClass('pressed')
+  }, 100)
+}
 
 /**
  *
  */
 const nextSequence = () => {
   //pick a random position number
-  let randomNumber = Math.floor(Math.random() * 4);
+  let randomNumber = Math.floor(Math.random() * 4)
 
   //pick a random color based on position
-  let randomChosenColour = buttonColours[randomNumber];
+  let randomChosenColour = buttonColours[randomNumber]
 
   //assign it to array
-  gamePattern.push(randomChosenColour);
+  gamePattern.push(randomChosenColour)
 
   // make a button flash
-  $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-  console.log(randomChosenColour);
+  $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
+  console.log(randomChosenColour)
 
   //addEVentListener when the button is clicked
   $(document).ready(function () {
     $('div[type="button"]').click(function (e) {
-      let userChosenColour = e.target.id;
+      let userChosenColour = e.target.id
 
-      userClickedPattern.push(userChosenColour);
-      console.log(userClickedPattern);
+      userClickedPattern.push(userChosenColour)
+      console.log(userClickedPattern)
       //add animated background to the button
-      animatePress(userChosenColour);
+      animatePress(userChosenColour)
 
       //play audio when button is clicked
-      playSound(userChosenColour);
-    });
-  });
-  return userClickedPattern;
-};
+      playSound(userChosenColour)
+    })
+  })
+  return userClickedPattern
+}
 
 //Call function
 
-nextSequence();
+let startToggle = true
+$(document).keypress(function (event) {
+  if (startToggle) {
+    $(`#${level - title}`).text('level 0')
+    nextSequence()
+    startToggle = false
+  }
+})
