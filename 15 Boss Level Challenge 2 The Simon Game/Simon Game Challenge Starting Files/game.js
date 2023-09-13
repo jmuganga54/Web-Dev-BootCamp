2b83da0ad87a3bd3430e7b5916ed71b0f72de5f6
joseph muganga <jmuganga54@users.noreply.github.com>
2023-09-13 04:44:36 +0300

    Step 7 Start the Game part 3

--- 15 Boss Level Challenge 2 The Simon Game/Simon Game Challenge Starting Files/game.js
+++ 15 Boss Level Challenge 2 The Simon Game/Simon Game Challenge Starting Files/game.js
@@ -4,6 +4,8 @@ let userClickedPattern = []
 let gamePattern = []
 let buttonColours = ['red', 'blue', 'green', 'yellow']
 let level = 0
+//get track if the game as started
+let started = false
 
 //functions
 /**
@@ -31,44 +33,39 @@ const animatePress = (currentColour) => {
  */
 const nextSequence = () => {
   //pick a random position number
+  level++
+  $('#level_title').text(`Level ${level}`)
+
   let randomNumber = Math.floor(Math.random() * 4)
 
   //pick a random color based on position
   let randomChosenColour = buttonColours[randomNumber]
-
   //assign it to array
   gamePattern.push(randomChosenColour)
 
   // make a button flash
   $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
-  console.log(randomChosenColour)
+  //play audio when button is clicked
+  playSound(randomChosenColour)
 
-  //addEVentListener when the button is clicked
-  $(document).ready(function () {
-    $('div[type="button"]').click(function (e) {
-      $('#level_title').text(`level ${level}`)
-      level++
-      let userChosenColour = e.target.id
-
-      userClickedPattern.push(userChosenColour)
-      console.log(userClickedPattern)
-      //add animated background to the button
-      animatePress(userChosenColour)
-
-      //play audio when button is clicked
-      playSound(userChosenColour)
-    })
-  })
   return userClickedPattern
 }
 
 //Call function
 
-let startToggle = true
 $(document).keypress(function (event) {
-  if (startToggle) {
+  if (!started) {
     $('#level_title').text(`level ${level}`)
     nextSequence()
-    startToggle = false
+    started = true
   }
+})
+
+//addEVentListener when the button is clicked
+$('div[type="button"]').click(function (e) {
+  let userChosenColour = e.target.id
+  userClickedPattern.push(userChosenColour)
+  console.log(userClickedPattern)
+  //add animated background to the button
+  animatePress(userChosenColour)
 })
